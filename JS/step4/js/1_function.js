2236771bfd1c4467282b1e195bc344f81dd7e6aa
Mark <teamofmark@gmail.com>
2020-05-27 09:27:49 +0900

    매개변수 arguments진행중

--- JS/step4/js/1_function.js
+++ JS/step4/js/1_function.js
@@ -41,3 +41,43 @@ function sumParam(num1, num2){
     var result = num1 + num2;
     alert("두 수의 합은 = " + result);
 }
+// sumParam(10,20);
+// ?  NaN(Not a Number).표현 불가능한 수치형 결과를 나타내는 값. 은 연산 과정에서 잘못된 입력을 받았음을 나타내는 기호
+
+// *exp.4 매개변수의 자리가 없어도 있는것처럼.. arguments
+// ? 가변인자함수 - 배열은 아니지만 배열처럼 사용가능.
+
+/*
+    ?arguments? 
+    arguments 객체는 모든 함수 안에서 사용하는 지역변수다.
+    arguments를 이용해 함수내 인수들을 참조 할 수 있고 이 객채의
+    함수에 전달된 각 인수항목을 포함한다.
+    단 arguments는 Array와 비슷해보이지만 length를 구하는것을 제외
+    Array와 다르다. 하지만 필요에 따라 Array로 변환하여 쓸수있다.
+*/
+
+// !ex.1
+function sumAll(){
+    alert(typeof (arguments) + ':' + arguments.length);
+}
+//? arguments는 배열같은 매개변수
+// sumAll(1,2,3,4,5,6,7,8,9);
+//? 함수를 호출할 때, 9개의 매개변수를 입력했기때문에, length가 9가 나온다.
+
+// !ex.2
+
+function sumAll2(){
+    var willReturn = 0;
+    for (var i in arguments){
+        //? for in 반복문은 IE9 이상에서만 가능. vue.js 등의 frontend framework에서 사용.
+        willReturn += arguments[i];
+        //? willReturn 이라는 변수에 arguments(매개변수의 배열) 내의 값을 더해서 대입해라. 배열의 갯수만큼.
+    }
+    return willReturn;
+    // 나온값을 리턴하라.
+}
+
+// *exp.5 매개변수 자리가 없는 곳에 입력된 매개변수를 arguments를 이용. 출력.
+function showInfo(){
+    
+}
